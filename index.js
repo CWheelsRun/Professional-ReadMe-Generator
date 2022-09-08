5197103f5c300c13978c0e448031334247dfccef
Corey B <corey.bennett@live.com>
2022-09-07 23:06:09 -0400

    ran npm install and began the file writing code

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const writeFile = require('./utils/generateFile.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(data => {
+        return generateMarkdown(data);
+    })
+    .then(pageMarkdown => {
+        return writeFile(pageMarkdown);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,27 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if(!license){
+    return '';
+  }
+  switch(license){
+    case 'MIT': return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'GPL v3': return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/)';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return '';
+  }
+  switch(license){
+    case 'MIT': return 'This project is covered under the [MIT License](https://opensource.org/licenses/MIT)';
+    case 'ISC': return 'This project is covered under the [ISC License](https://opensource.org/licenses/ISC)';
+    case 'GPL v3': return 'This project is covered under the [GNU General Public License v3.0](https://www.gnu.org/licenses/)';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
